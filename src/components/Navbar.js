67f5db7c9bf05fe63baae41498ec7c7c7a2e4c1e
Dinar Davis <dinardavis@gmail.com>
2022-08-30 17:29:47 +0300

    Add hide/unhide navbar feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,44 @@
-import React from "react";
+
+import React from 'react';
+import { debounce } from './utilities/helpers';
 
 export default function Navbar() {
+  const [prevScroll, setPrevScroll] = React.useState(0) 
+  const [visible, setVisible] = React.useState(false)
 
+  const handleScroll = debounce(() => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    if(scrollTop > prevScroll) {
+      setVisible(false)
+    } else {
+      setVisible(true)
+    }
+    setPrevScroll(scrollTop)
+  }, 10)
+
+  console.log(prevScroll)
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+
+  }, [prevScroll, visible, handleScroll])
+
+  const styles = {
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 20px',
+    zIndex: '100',
+    width: '100%',
+    transition: 'top 0.4s',
+  }
+
+  console.log(prevScroll === 0)
+  
   return (
-    <header className="nav-container">
+    <header className={`nav-container ${prevScroll === 0 ? "" : "nav-shadow"}`} style = {{...styles, top: visible ? '0px' : '-75px'}}>
       <a href="/#">
         <img className="nav-logo" src="imgs/logo.png" alt="" />
       </a>
