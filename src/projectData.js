a05794979ec25daf40f67592edaa5d9b798ecca2
Dinar Davis <dinardavis@gmail.com>
2022-11-09 16:32:51 +0700

    Test project section update

--- src/projectData.js
+++ src/projectData.js
@@ -26,6 +26,15 @@ const projectData = [
     gitURL: "https://github.com/dinardavis/spacex_clone",
     liveDemoURL: "https://dinardavis.github.io/spacex_clone/"
   },
+  {
+    id: 4,
+    imageURL: "url(https://dinardavis.github.io/portfolio_v1/imgs/spacex_thumbnail.png)",
+    title: "SpaceX Landing Page",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quo ea odit perspiciatis vel fugit repellendus consectetur inventore, voluptates natus excepturi accusantium nesciunt autem cumque veritatis alias quos eum nulla!",
+    tech: ["HTML","CSS","JavaScript"],
+    gitURL: "https://github.com/dinardavis/spacex_clone",
+    liveDemoURL: "https://dinardavis.github.io/spacex_clone/"
+  },
 ]
 
 export default projectData
