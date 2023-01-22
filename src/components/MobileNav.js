533e1f6b6c30a80bad78573e7e991d5adf22c537
Dinar Davis <dinardavis@gmail.com>
2023-01-22 09:25:25 +0700

    Add social links to mobile nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -21,7 +21,12 @@ export default function MobileNav(props) {
         <a href="#about" className="mobile-nav-link">about</a>
         <a href="#work" className="mobile-nav-link">projects</a>
         <a href="#contact" className="mobile-nav-link">contact</a>
-        <a href={require("../assets/dinardavis.pdf")} download="dinardavis" className="mobile-nav-link">resumé</a>
+        <div className="socials-mobile-nav">
+          <a href="https://github.com/dinardavis" className="social-link" target="_blank" rel="noopener noreferrer"><img src="imgs/social/github.svg" alt=""/></a>
+          <a href="https://www.linkedin.com/in/dinardavis/" className="social-link" target="_blank" rel="noopener noreferrer"><img src="imgs/social/linkedin.svg" alt=""/></a>
+          <a href="https://twitter.com/dinardavis" className="social-link" target="_blank" rel="noopener noreferrer"><img src="imgs/social/twitter.svg" alt=""/></a>
+          <a href={require("../assets/dinardavis.pdf")} download="dinardavis" className="social-link" target="_blank" rel="noopener noreferrer"><img src="imgs/social/cv.svg" title="Download Resumé" alt="Resumé Logo"/></a>
+        </div>
         <div className="mobile-nav-link dark-mode-toggle" onClick={props.toggleDarkMode}>{props.darkMode ? <i className="fa-regular fa-lightbulb"></i> : <i className="fa-solid fa-lightbulb"></i>}</div>
       </div>
     </div>
